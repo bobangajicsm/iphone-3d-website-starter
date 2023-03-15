b476ed2202e01dc7dd6bac9e9dfb43bfd5e4f562
milliorn <scottmilliorn@gmail.com>
2023-03-14 20:05:48 -0700

    animate, opacity

--- src/lib/scroll-animation.js
+++ src/lib/scroll-animation.js
@@ -2,4 +2,54 @@ import gsap from "gsap";
 
 export const scrollAnimation = (position, target, onUpdate) => {
   const timeline = gsap.timeline();
+
+  timeline
+    .to(position, {
+      x: -3.38,
+      y: -10.74,
+      z: -5.93,
+      scrollTrigger: {
+        trigger: ".sound-section",
+        start: "top bottom",
+        end: "top top",
+        scrub: 2,
+        immediateRender: false,
+      },
+      onUpdate,
+    })
+    .to(target, {
+      x: 1.52,
+      y: 0.77,
+      z: -1.08,
+      scrollTrigger: {
+        trigger: ".sound-section",
+        start: "top bottom",
+        end: "top top",
+        scrub: 2,
+        immediateRender: false,
+      },
+      onUpdate,
+    })
+    .to(".jumbotron-section", {
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".sound-section",
+        start: "top bottom",
+        end: "top top",
+        scrub: 2,
+        immediateRender: false,
+      },
+      onUpdate,
+    })
+    .to(".sound-section-content", {
+      opacity: 1,
+      scrollTrigger: {
+        trigger: ".sound-section",
+        start: "top bottom",
+        end: "top top",
+        scrub: 2,
+        immediateRender: false,
+      },
+      onUpdate,
+    });
 };
