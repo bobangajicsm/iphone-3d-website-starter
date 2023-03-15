62c11ceb8eba2c553475ff46dc287ce1681b51fd
milliorn <scottmilliorn@gmail.com>
2023-03-14 20:25:36 -0700

    scrollAnimation, opacity

--- src/lib/scroll-animation.js
+++ src/lib/scroll-animation.js
@@ -28,7 +28,6 @@ export const scrollAnimation = (position, target, onUpdate) => {
         scrub: 2,
         immediateRender: false,
       },
-      onUpdate,
     })
     .to(".jumbotron-section", {
       opacity: 0,
@@ -39,7 +38,6 @@ export const scrollAnimation = (position, target, onUpdate) => {
         scrub: 2,
         immediateRender: false,
       },
-      onUpdate,
     })
     .to(".sound-section-content", {
       opacity: 1,
@@ -50,6 +48,41 @@ export const scrollAnimation = (position, target, onUpdate) => {
         scrub: 2,
         immediateRender: false,
       },
+    })
+    .to(position, {
+      x: 1.56,
+      y: 5.0,
+      z: 0.01,
+      scrollTrigger: {
+        trigger: ".display-section",
+        start: "top bottom",
+        end: "top top",
+        scrub: 2,
+        immediateRender: false,
+      },
       onUpdate,
+    })
+    .to(target, {
+      x: -0.55,
+      y: 0.32,
+      z: 0.0,
+      scrollTrigger: {
+        trigger: ".display-section",
+        start: "top bottom",
+        end: "top top",
+        scrub: 2,
+        immediateRender: false,
+      },
+      onUpdate,
+    })
+    .to(".display-section", {
+      opacity: 1,
+      scrollTrigger: {
+        trigger: ".display-section",
+        start: "top bottom",
+        end: "top top",
+        scrub: 2,
+        immediateRender: false,
+      },
     });
 };
