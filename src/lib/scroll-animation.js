766ebb43068b84ff8617a92a4700fd99bc037c59
milliorn <scottmilliorn@gmail.com>
2023-03-15 00:10:03 -0700

    update coords

--- src/lib/scroll-animation.js
+++ src/lib/scroll-animation.js
@@ -1,13 +1,13 @@
 import gsap from "gsap";
 
-export const scrollAnimation = (position, target, onUpdate) => {
+export const scrollAnimation = (position, target, isMobile, onUpdate) => {
   const timeline = gsap.timeline();
 
   timeline
     .to(position, {
-      x: -3.38,
-      y: -10.74,
-      z: -5.93,
+      x: !isMobile ? -3.38 : -7.0,
+      y: !isMobile ? -10.74 : -12.2,
+      z: !isMobile ? -5.93 : -6.0,
       scrollTrigger: {
         trigger: ".sound-section",
         start: "top bottom",
@@ -18,9 +18,9 @@ export const scrollAnimation = (position, target, onUpdate) => {
       onUpdate,
     })
     .to(target, {
-      x: 1.52,
-      y: 0.77,
-      z: -1.08,
+      x: !isMobile ? 1.52 : 0.7,
+      y: !isMobile ? 0.77 : 1.9,
+      z: !isMobile ? -1.08 : 0.7,
       scrollTrigger: {
         trigger: ".sound-section",
         start: "top bottom",
@@ -50,9 +50,9 @@ export const scrollAnimation = (position, target, onUpdate) => {
       },
     })
     .to(position, {
-      x: 1.56,
-      y: 5.0,
-      z: 0.01,
+      x: !isMobile ? 1.56 : 9.36,
+      y: !isMobile ? 5.0 : 10.95,
+      z: !isMobile ? 0.01 : 0.09,
       scrollTrigger: {
         trigger: ".display-section",
         start: "top bottom",
@@ -63,9 +63,9 @@ export const scrollAnimation = (position, target, onUpdate) => {
       onUpdate,
     })
     .to(target, {
-      x: -0.55,
-      y: 0.32,
-      z: 0.0,
+      x: !isMobile ? -0.55 : -1.62,
+      y: !isMobile ? 0.32 : 0.02,
+      z: !isMobile ? 0.0 : -0.06,
       scrollTrigger: {
         trigger: ".display-section",
         start: "top bottom",
