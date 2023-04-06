54f5b71eb6acab528890708412dca472d1e1f820
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-04-06 00:03:56 -0500

    navbar display

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Nav from "./components/Nav";
+
 
 function App() {
 
   return (
     <div className="App">
-      Hello
+      <Nav/>
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Logo from "../assets/images/logo.svg";
 
 function Nav() {
-    return ( <nav>
-        
-    </nav> );
+  return (
+    <nav className="nav-wrapper">
+      <div className="nav-content">
+        <ul className="list-styled">
+          <li>
+            <img src={Logo} alt="Apple" />
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Nav;
