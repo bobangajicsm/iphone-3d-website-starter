b53260a87d6287b58aa6207ba023de8e80876ff7
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-04-06 12:17:03 -0500

    logo and buy button

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -5,7 +5,19 @@ function Jumbotron() {
     return (
         <div className="jumbotron-section wrapper">
             <h2 className="title">New</h2>
-            <img className="logo" src={Iphone} alt="iPhone 14 Pro"/>
+            <img className="logo" src={Iphone} alt="iPhone 14 Pro"/> 
+            <p className="text">Big and bigger</p>
+            <span className="description">
+                From $41.62/mo. for 24 mo. or $999 before trade-in
+            </span>
+            <ul className="links">
+                <li>
+                    <button className="button">Buy</button>
+                </li>
+                <li>
+                    <a className="link">Learn more</a>
+                </li>
+            </ul>
         </div>
       );
 }
