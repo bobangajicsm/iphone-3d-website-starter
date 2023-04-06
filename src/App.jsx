b529683e68af54046122206cf6920692f58de4c7
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-04-06 13:42:46 -0500

    3d model import

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Nav from "./components/Nav";
 import Jumbotron from "./components/Jumbotron";
 import SoundSection from "./components/SoundSection";
 import DisplaySection from './components/DisplaySection'
+import WebgiViewer from "./components/WebgiViewer";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Jumbotron />
       <SoundSection />
       <DisplaySection />
+      <WebgiViewer />
     </div>
   );
 }
--- src/components/WebgiViewer.jsx
+++ src/components/WebgiViewer.jsx
@@ -1,81 +1,84 @@
-import React, {useRef, useState, useCallback, useImperativeHandle} from "react";
+import React, {
+  useRef,
+  useState,
+  useCallback,
+  useImperativeHandle,
+  useEffect,
+} from "react";
 import {
-    ViewerApp,
-    AssetManagerPlugin,
-    GBufferPlugin,
-    ProgressivePlugin,
-    TonemapPlugin,
-    SSRPlugin,
-    SSAOPlugin,
-    BloomPlugin, 
-    GammaCorrectionPlugin,
-    addBasePlugins,
-    mobileAndTabletCheck,
+  ViewerApp,
+  AssetManagerPlugin,
+  GBufferPlugin,
+  ProgressivePlugin,
+  TonemapPlugin,
+  SSRPlugin,
+  SSAOPlugin,
+  BloomPlugin,
+  GammaCorrectionPlugin,
+  addBasePlugins,
+  mobileAndTabletCheck,
+  CanvasSnipperPlugin,
 } from "webgi";
-import gsap from 'gsap'
+import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 function WebgiViewer() {
-    const canvasRef = useRef(null);
+  const canvasRef = useRef(null);
 
-    async function setupViewer(){
+  const setupViewer = useCallback(async () => {
+    {
+      // Initialize the viewer
+      const viewer = new ViewerApp({
+        canvas: canvasRef.current,
+      });
 
-        // Initialize the viewer
-        const viewer = new ViewerApp({
-            canvas: document.getElementById('webgi-canvas') as HTMLCanvasElement,
-        })
-    
-        // Add some plugins
-        const manager = await viewer.addPlugin(AssetManagerPlugin)
-    
-        // Add a popup(in HTML) with download progress when any asset is downloading.
-        await viewer.addPlugin(AssetManagerBasicPopupPlugin)
-    
-        // Add plugins individually.
-        // await viewer.addPlugin(GBufferPlugin)
-        // await viewer.addPlugin(new ProgressivePlugin(32))
-        // await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
-        // await viewer.addPlugin(GammaCorrectionPlugin)
-        // await viewer.addPlugin(SSRPlugin)
-        // await viewer.addPlugin(SSAOPlugin)
-        // await viewer.addPlugin(DiamondPlugin)
-        // await viewer.addPlugin(FrameFadePlugin)
-        // await viewer.addPlugin(GLTFAnimationPlugin)
-        // await viewer.addPlugin(GroundPlugin)
-        // await viewer.addPlugin(BloomPlugin)
-        // await viewer.addPlugin(TemporalAAPlugin)
-        // await viewer.addPlugin(AnisotropyPlugin)
-    
-        // or use this to add all main ones at once.
-        await addBasePlugins(viewer)
-    
-        // Add more plugins not available in base, like CanvasSnipperPlugin which has helpers to download an image of the canvas.
-        await viewer.addPlugin(CanvasSnipperPlugin)
-    
-        // This must be called once after all plugins are added.
-        viewer.renderer.refreshPipeline()
-    
-        await manager.addFromPath("./assets/classic-watch.glb")
-    
-        // Load an environment map if not set in the glb file
-        // await viewer.scene.setEnvironment(
-        //     await manager.importer!.importSinglePath<ITexture>(
-        //         "./assets/environment.hdr"
-        //     )
-        // );
-    
-        // Add some UI for tweak and testing.
-        const uiPlugin = await viewer.addPlugin(TweakpaneUiPlugin)
-        // Add plugins to the UI to see their settings.
-        uiPlugin.setupPlugins<IViewerPlugin>(TonemapPlugin, CanvasSnipperPlugin)
-    
+      // Add some plugins
+      const manager = await viewer.addPlugin(AssetManagerPlugin);
+
+      // Add plugins individually.
+      // await viewer.addPlugin(GBufferPlugin)
+      // await viewer.addPlugin(new ProgressivePlugin(32))
+      // await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
+      // await viewer.addPlugin(GammaCorrectionPlugin)
+      // await viewer.addPlugin(SSRPlugin)
+      // await viewer.addPlugin(SSAOPlugin)
+      // await viewer.addPlugin(DiamondPlugin)
+      // await viewer.addPlugin(FrameFadePlugin)
+      // await viewer.addPlugin(GLTFAnimationPlugin)
+      // await viewer.addPlugin(GroundPlugin)
+      // await viewer.addPlugin(BloomPlugin)
+      // await viewer.addPlugin(TemporalAAPlugin)
+      // await viewer.addPlugin(AnisotropyPlugin)
+
+      // or use this to add all main ones at once.
+      await addBasePlugins(viewer);
+
+      // Add more plugins not available in base, like CanvasSnipperPlugin which has helpers to download an image of the canvas.
+      await viewer.addPlugin(CanvasSnipperPlugin);
+
+      // This must be called once after all plugins are added.
+      viewer.renderer.refreshPipeline();
+
+      await manager.addFromPath("scene-black.glb");
+
+      // Load an environment map if not set in the glb file
+      // await viewer.scene.setEnvironment(
+      //     await manager.importer!.importSinglePath<ITexture>(
+      //         "./assets/environment.hdr"
+      //     )
+      // );
     }
+  }, []);
 
-    return ( 
+  useEffect(() => {
+    setupViewer();
+  }, [])
+
+  return (
     <div id="webgi-canvas-container">
-        <canvas id="webgi-canvas" ref={canvasRef} />
-    </div> 
-    );
+      <canvas id="webgi-canvas" ref={canvasRef} />
+    </div>
+  );
 }
 
 export default WebgiViewer;
