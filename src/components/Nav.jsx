52e893948a8e5cb0542d360ce6e040fe45a657f4
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-04-06 00:10:01 -0500

    update navbar links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Logo from "../assets/images/logo.svg";
+import Search from '../assets/images/search.svg'
+import Store from '../assets/images/store.svg'
 
 function Nav() {
   return (
@@ -8,6 +10,42 @@ function Nav() {
         <ul className="list-styled">
           <li>
             <img src={Logo} alt="Apple" />
+          </li>
+          <li>
+            <a className="link-styled">Store</a>
+          </li>
+          <li>
+            <a className="link-styled">Mac</a>
+          </li>
+          <li>
+            <a className="link-styled">iPad</a>
+          </li>
+          <li>
+            <a className="link-styled">iPhone</a>
+          </li>
+          <li>
+            <a className="link-styled">Watch</a>
+          </li>
+          <li>
+            <a className="link-styled">AirPods</a>
+          </li>
+          <li>
+            <a className="link-styled">TV & Home</a>
+          </li>
+          <li>
+            <a className="link-styled">Entertainment</a>
+          </li>
+          <li>
+            <a className="link-styled">Accessories</a>
+          </li>
+          <li>
+            <a className="link-styled">Support</a>
+          </li>
+          <li>
+            <img src={Search} alt="Search" />
+          </li>
+          <li>
+            <img src={Store} alt="Store" />
           </li>
         </ul>
       </div>
