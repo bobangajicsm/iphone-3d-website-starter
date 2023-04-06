5ba41fd346fd42a9ab1c8112d8fb13297f86735a
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-04-06 12:36:58 -0500

    learn more scroll handler

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -3,6 +3,15 @@ import Iphone from "../assets/images/iphone-14.jpg";
 import HoldingIphone from "../assets/images/iphone-hand.png";
 
 function Jumbotron() {
+  const handleLearnMore = () => {
+    const element = document.querySelector(".sound-section");
+    window.scrollTo({
+      top: element?.getBoundingClientRect().top,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div className="jumbotron-section wrapper">
       <h2 className="title">New</h2>
@@ -16,7 +25,7 @@ function Jumbotron() {
           <button className="button">Buy</button>
         </li>
         <li>
-          <a className="link">Learn more</a>
+          <a onClick={handleLearnMore} className="link">Learn more</a>
         </li>
       </ul>
       <img className="iphone-img" src={HoldingIphone} alt="iPhone" />
